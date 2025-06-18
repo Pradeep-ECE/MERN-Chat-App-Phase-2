c4bbe5ad89ea01633da9671918a37c9af4336440
Pradeep-M <pradeepm@centizen.com>
2025-06-18 09:14:13 +0530

    ci/cd changes

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const { app, server } = require('./socket/index')
 
 // const app = express()
 app.use(cors({
-    origin : process.env.FRONTEND_URL,
+    origin : 'http://localhost:3001',
     credentials : true
 }))
 app.use(express.json())
